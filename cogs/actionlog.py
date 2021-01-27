098279022c219a69e0de28806c1cf5589b949d3a
xfazze <blixtvinge@gmail.com>
2021-01-27 19:23:53 +0100

    mege

--- cogs/actionlog.py
+++ cogs/actionlog.py
@@ -9,7 +9,6 @@ class Base(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        print(before, "\n", after)
         action = "admin abuse"
         if before.self_mute != after.self_mute:
             if before.self_mute:
@@ -40,9 +39,50 @@ class Base(commands.Cog):
         
         
         with open('/home/pi/discordbot/vc_logs.txt', 'a') as f:
-            print(str(member)+" "+action)
-            f.write(str(member)+" "+action + "\n")
+            f.write(str(time.time())+" "+str(member)+" "+action + "\n")
 
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        member = await message.guild.fetch_member(message.author.id)
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " send "   + str(message.guild.id)+ " "+ str(message.channel.id)+ " "+str(message.id) +" "+ str(member) + " "+ "\n")
 
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, payload):
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " delete " + str(payload.guild_id) +" "+ str(payload.channel_id) +" "+ str(payload.message_id) + " "+ "\n")
+            
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, payload):
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " edit " + str(payload.data["guild_id"]) + " " + str(payload.data["channel_id"]) +" "+ str(payload.data["id"]) + " "+ str(payload.data["author"]["username"])+ "#"+str(payload.data["author"]["discriminator"]) + "\n")
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " reactadd " + str(payload.guild_id) +" "+ str(payload.channel_id) +" "+ str(payload.message_id) + " " + str(payload.member.name) +"#"+ str(payload.member.discriminator) + " " + str(payload.emoji.name) +"\n")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        print(payload)
+        guild = self.bot.get_guild(payload.guild_id)
+        member = await guild.fetch_member(payload.user_id)
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " reactremove " + str(payload.guild_id) +" "+ str(payload.channel_id) +" "+ str(payload.message_id) + " " + str(member) + " " + str(payload.emoji.name) +"\n")
+
+    @commands.Cog.listener()
+    async def on_typing(self, channel, user, when):        
+        with open('/home/pi/discordbot/tc_logs.txt', 'a') as f:
+            f.write(str(time.time()) + " typing "  +str(channel.guild.id) + " "+ str(channel.id) +" " +str(user)+"\n")
+
+# on yuypine
+
+# channel create(private), channel delete, channel update
+# role create, role delete, role update
+# guild join, guild remove, guild update
+# guild emoji update
+# ban unban
+# invite create remove
 def setup(bot):
     bot.add_cog(Base(bot))

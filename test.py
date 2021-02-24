89d297403ae00e4c7c269cffad3acb937f4b1bb1
xfazze <blixtvinge@gmail.com>
2021-02-24 10:11:36 +0100

    mmee

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from PIL import Image
-
-url  = 'https://cdn.discordapp.com/emojis/587726828815384738.png?v=1'
-
-
-im = Image.open(requests.get(url, stream=True).raw)
-im.show()
-print(type(im))

88235dc398670fcb1ca05b9f04f3d6c4940bb38d
xfazze <blixtvinge@gmail.com>
2021-03-14 08:53:09 +0100

    hej

--- cogs/playq.py
+++ cogs/playq.py
@@ -1,8 +1,11 @@
 import discord
 import ffmpeg
+import youtube_dl
+import os
+from os import system
 from discord.utils import get
 from discord.ext import commands
-
+from discord import FFmpegPCMAudio
 
 class Base(commands.Cog):
     def __init__(self, bot):
@@ -13,16 +16,38 @@ class Base(commands.Cog):
     async def play(self,ctx):
         channel = ctx.message.author.voice.channel
         print(self.bot.voice_clients, ctx.guild)
-        voice = get(self.bot.voice_clients, guild=ctx.guild)
-        path = r"/home/pi/discordbot/songs/djungeltrubbaduren.mp3"
         if voice and voice.is_connected():
             await voice.move_to(channel)
         else:
             voice = await channel.connect()
             await ctx.send("God has entered the chat")
-        vc = get(self.bot.voice_clients, guild=ctx.guild)
-        vc.play(discord.FFmpegPCMAudio(path),
-                after=lambda e: print("song is done"))
-        vc.source = discord.PCMVolumeTransformer(vc.source)
+
+        song_there = os.path.isfile("song.mp3")
+        try:
+            if song_there:
+                os.remove("song.mp3")
+        except PermissionError:
+            await ctx.send("Wait for the current playing music end or use the 'stop' command")
+            return
+        await ctx.send("Getting everything ready, playing audio soon")
+        print("Someone wants to play music let me get that ready for them...")
+        voice = get(bot.voice_clients, guild=ctx.guild)
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, 'song.mp3')
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        voice.volume = 100
+        voice.is_playing()
+        
 def setup(bot):
     bot.add_cog(Base(bot))

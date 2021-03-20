e8b7b833b2249e4ed3afe2a0647ae0fb2bfcf408
xfazze <blixtvinge@gmail.com>
2021-03-20 13:24:46 +0100

    maslog

--- cogs/maslog.py
+++ cogs/maslog.py
@@ -12,6 +12,7 @@ class Base(commands.Cog):
         if message.content[0:3] == ",,," and message.author.id == 243022798543519745:
             with open('/home/pi/discordbot/maslog.txt', 'a') as f:
                 f.write(str(time.time()) + " "+ message.content[3:]+"\n")
+            await message.delete()
 
 def setup(bot):
     bot.add_cog(Base(bot))

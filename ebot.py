74ecae9e0a08ad6923eb7b64a5f0cc2f94297a47
xfazze <blixtvinge@gmail.com>
2021-04-13 12:40:20 +0200

    push

--- ebot.py
+++ ebot.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix=determine_prefix, intents=intents)
 bot.remove_command('help')
 
 
-extensions = ['cogs.admin.joinleavemessage', 'cogs.logging.joinleavelog']
+extensions = ['cogs.voice.bettervc']
 
 
 

3c582bad0f82311d2e8c7d66e13dc795dff1e345
xfazze <blixtvinge@gmail.com>
2021-05-11 03:41:10 +0200

    back

--- masloganalyze.py
+++ masloganalyze.py
@@ -5,10 +5,13 @@
 # avg time/day per (overall, month, weekday)
 # how many per hour (overall, month, weekday)
 
-
+x = 0
 with open('/home/pi/discordbot/maslog.txt', 'r') as f:
     lines = f.readlines()
 nlines =[]
 for line in lines:
+    x += 1
+    print(line)
     nlines.append(line[:-1].split(" ", 1))
-print(nlines)
+#print(nlines)
+print(x)

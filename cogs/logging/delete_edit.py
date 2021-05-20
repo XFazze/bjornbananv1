2958e5e04622847ff582f0590ec737e2c16d6816
xfazze <blixtvinge@gmail.com>
2021-05-20 15:36:46 +0200

    hjej

--- cogs/logging/delete_edit.py
+++ cogs/logging/delete_edit.py
@@ -1,9 +1,7 @@
 import discord
 import json
-import math
 import time
 import os
-from datetime import datetime
 from discord.ext import commands
 
 
@@ -17,43 +15,9 @@ class Base(commands.Cog):
             delete_logs = json.load(f)
             sending_message = 'Deleted messages in this channel\n'
             for message in delete_logs[str(ctx.guild.id)][str(ctx.channel.id)]:
-                print()
                 sending_message = sending_message +"\n" + delete_logs[str(ctx.guild.id)][str(ctx.channel.id)][message]["time"] + " " + delete_logs[str(ctx.guild.id)][str(ctx.channel.id)][message]['author_name'] + "#" + delete_logs[str(ctx.guild.id)][str(ctx.channel.id)][message]['author_discriminator']  + ":\n" + delete_logs[str(ctx.guild.id)][str(ctx.channel.id)][message]['content']
-
+            sending_message = "```" + sending_message + "```"
             await ctx.send(sending_message)
-
-
-
-    @commands.Cog.listener()
-    async def on_message(self, ctx):
-        jsonf = {
-                    "content" : str(ctx.content),
-                    "author_name" : str(ctx.author.name),
-                    "author_discriminator" : str(ctx.author.discriminator),
-                    "time" : str(datetime.now())
-                }
-        filename = '/home/pi/discordbot/logs/message_logs/' + \
-            str(math.floor(time.time()/86400))+'.json'
-        try:
-            with open(filename, 'r') as f:
-                deletemessages = json.load(f)
-                if str(ctx.guild.id) not in deletemessages.keys():
-                    print("created guild dict")
-                    deletemessages[str(ctx.guild.id)]={}
-                if str(ctx.channel.id) not in deletemessages[str(ctx.guild.id)].keys():
-                    print("created channel dict")
-                    deletemessages[str(ctx.guild.id)][str(ctx.channel.id)] ={}
-                
-                deletemessages[str(ctx.guild.id)][str(ctx.channel.id)][str(ctx.id)] = jsonf
-            with open(filename, 'w') as f:
-                json.dump(deletemessages, f, indent=4)
-
-        except:
-            print("created file")
-            with open(filename, 'w') as f:
-                jsonfile = { str(ctx.guild.id) : {str(ctx.channel.id): {str(ctx.id) : jsonf}}}
-                json.dump(jsonfile, f, indent=4)
-
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, ctx):
@@ -88,6 +52,6 @@ class Base(commands.Cog):
 
         pass
         
-    
+
 def setup(bot):
     bot.add_cog(Base(bot))

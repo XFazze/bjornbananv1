2ac752b1e78dd2a870244f9907c745e513fb444a
fabian <41339139+XFazze@users.noreply.github.com>
2021-08-13 20:58:07 +0200

    sd

--- mainbot.py
+++ mainbot.py
@@ -24,7 +24,7 @@ bot.remove_command('help')
 
 extensions = ['cogs.admin.deletingchannel', 'cogs.admin.enabledisable', 'cogs.admin.joinroles', 'cogs.admin.reaction_roles', 'cogs.admin.ticket', 'cogs.admin.joinleavemessage',
               'cogs.logging.actionlog', 'cogs.logging.rolelog', 'cogs.logging.tcstats', 'cogs.logging.vcstats', 'cogs.logging.joinleavelog', 'cogs.random.dnd', 'cogs.random.maslog',
-              'cogs.random.shroud', 'cogs.random.simple', 'cogs.voice.basic_vc', 'cogs.voice.bettervc', 'cogs.logging.delete_edit', 'cogs.logging.messagelog', 'cogs.logging.delete_log', 'cogs.random.random']
+              'cogs.random.shroud', 'cogs.random.simple', 'cogs.voice.basic_vc', 'cogs.voice.bettervc', 'cogs.logging.delete_edit', 'cogs.logging.messagelog', 'cogs.logging.delete_log', 'cogs.random.todo']
 
 
 if __name__ == '__main__':

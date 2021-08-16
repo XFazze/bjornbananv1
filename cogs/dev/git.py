70720b8af8f67f861f0c4e0e269f9041bf0ab68d
Noel Kleen <noelhkleen@gmail.com>
2021-08-16 14:48:49 +0200

    g

--- cogs/dev/git.py
+++ cogs/dev/git.py
@@ -1,19 +1,20 @@
 import discord
 from discord.ext import commands
-import subprocess
 import git
 
-
+g = git.cmd.Git("")
 
 class Git(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def git(self, ctx, action):
-        if action == "pull":
-            g = git.cmd.Git("")
-            result = g.pull()
-            print(result)
+    async def git(self, ctx, action = None):
+        if action == None:
+            await ctx.send("Specify an action")
+        elif action == "pull":
+            print("bitchhhh")
+            #result = g.pull()
+            #print(result)
     
 def setup(bot):
     bot.add_cog(Git(bot))

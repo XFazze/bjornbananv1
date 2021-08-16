9649b1531a8884b76d6e5c8512ba7031d3dc3371
Noel Kleen <noelhkleen@gmail.com>
2021-08-16 15:29:06 +0200

    fixed git cog

--- cogs/dev/git.py
+++ cogs/dev/git.py
@@ -17,9 +17,17 @@ class Git(commands.Cog):
         
             
         if action == None:
-            await ctx.send("Specify an action")
+            embed = discord.Embed(title="Specify an action.", color=0xFD3333)
+            await ctx.send(embed=embed)
+        elif action == "pull":
+            s = g.pull()
+            embed = discord.Embed(color=0x00FF42)
+            embed.add_field(name="Git pull", value=s)
+            await ctx.send(embed=embed)
         else:
-            await ctx.send("Success")
-    
+            embed = discord.Embed(title="Action does not exists.", color=0xFD3333)
+            await ctx.send(embed=embed)
+            
+            
 def setup(bot):
     bot.add_cog(Git(bot))

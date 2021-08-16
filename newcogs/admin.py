71b9c9814d087bc4c8da4a782603b9ba1a46caa5
xfazze <a@fabbe90.gq>
2021-08-16 23:53:23 +0200

    joinrole fixed to mongodb

--- newcogs/admin.py
+++ newcogs/admin.py
@@ -1,5 +1,6 @@
 import discord
 import json
+from discord import member
 from discord.utils import get
 from pymongo import MongoClient, collation
 from discord.ext import commands, tasks
@@ -8,31 +9,23 @@ import os
 import pymongo as pm
 
 
-
-
-
-
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 
-
-
-
-
 # Enable/disable command
-    
+
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_roles=True)
-    async def disable(self,ctx):
+    async def disable(self, ctx):
         try:
             command = str(ctx.message.content).split(" ")[1]
         except:
             await ctx.send("Provide a command")
         if command in ["enable", "disable"]:
             await ctx.send("cant disable this command")
-        for botcommand in  self.bot.commands:
+        for botcommand in self.bot.commands:
             if command == str(botcommand):
                 guild_id = ctx.guild.id
                 with open('/tmp/discordbot/management/enable.json', 'r+') as f:
@@ -51,17 +44,16 @@ class Admin(commands.Cog):
                         with open('/tmp/discordbot/management/enable.json', 'w') as file:
                             json.dump(enable, file, indent=4)
 
-    
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_roles=True)
-    async def enable(self,ctx):
+    async def enable(self, ctx):
         try:
             command = str(ctx.message.content).split(" ")[1]
         except:
             await ctx.send("Provide a command")
         if command in ["enable", "disable"]:
             await ctx.send("cant disable this command")
-        for botcommand in  self.bot.commands:
+        for botcommand in self.bot.commands:
             if command == str(botcommand):
                 guild_id = ctx.guild.id
                 with open('/tmp/discordbot/management/enable.json', 'r+') as f:
@@ -88,7 +80,7 @@ class Admin(commands.Cog):
         await ctx.send("Command does not exist")
 
 # Björnbanan set prefix
-    
+
     @commands.Cog.listener()
     @commands.has_permissions(manage_guild=True)
     async def on_message(self, message):
@@ -96,10 +88,11 @@ class Admin(commands.Cog):
         if msg[0:19] != "bjornbanansetprefix":
             return
         if len(msg.split(' ')) != 2:
-            embed = discord.Embed(title="Prove a valid prefix. bjornbanansetprefix [prefix]", color=0xFD3333)
+            embed = discord.Embed(
+                title="Prove a valid prefix. bjornbanansetprefix [prefix]", color=0xFD3333)
             await message.channel.send(embed=embed)
             return
-        
+
         prefix = msg.split(' ')[1]
 
         collection = MongoClient('localhost', 27017).maindb.guilds
@@ -109,92 +102,88 @@ class Admin(commands.Cog):
         config["prefix"] = prefix
         newvalue = {"$set": {"config": config}}
         collection.update_one(myquery, newvalue)
-        embed = discord.Embed(title="Successfully changed the prefix to "+ prefix, color=0x00FF42)
+        embed = discord.Embed(
+            title="Successfully changed the prefix to " + prefix, color=0x00FF42)
         await message.channel.send(embed=embed)
 
 # Join roles
 
     @commands.command(pass_context=True, aliases=['jra', 'jradd'])
     @commands.has_permissions(manage_roles=True)
-    async def joinroleadd(self, ctx):
-        try:
-            role_id = int(str(ctx.message.content).split(" ")[1][3:-1])
-        except:
-            await ctx.send("you forgot the role")
+    async def joinroleadd(self, ctx, role: discord.Role = None):
+        if role == None:
+            embed = discord.Embed(title="Provide a role", color=0xFD3333)
+            await ctx.send(embed=embed)
+            return
+        if ctx.author.top_role.position < role.position:
+            await ctx.send(embed=discord.Embed(title="You dont have the permissions to add this role", color=0xFD3333))
             return
 
-        guild_id = ctx.message.guild.id
-        with open('/tmp/discordbot/management/joinrole.json', 'r+') as f:
-            joinrole = json.load(f)
-            if str(guild_id) in joinrole.keys():
-                if role_id in joinrole[str(guild_id)]:
-                    await ctx.send("This channel is already added to joinrole")
-                else:
-                    joinrole[str(guild_id)].append(role_id)
-                    await ctx.send("Added channel to joinrole")
-                    with open('/tmp/discordbot/management/joinrole.json', 'w') as file:
-                        json.dump(joinrole, file, indent=4)
-            else:
-                joinrole[int(guild_id)] = [role_id]
-                await ctx.send("Added channel to joinrole")
-                with open('/tmp/discordbot/management/joinrole.json', 'w') as file:
-                    json.dump(joinrole, file, indent=4)
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        config = collection.find_one(myquery)["config"]
 
+        if role.id in config["joinrole"]:
+            await ctx.send(embed=discord.Embed(title="This role is already added to joinrole", color=0xFD3333))
+            return
+        else:
+            config["joinrole"].append(role.id)
+            await ctx.send(embed=discord.Embed(title="Added role to joinrole", color=0x00FF42))
+
+        newvalue = {"$set": {"config": config}}
+        collection.update_one(myquery, newvalue)
 
     @commands.command(pass_context=True, aliases=['jrr', 'jrremove'])
     @commands.has_permissions(manage_roles=True)
-    async def joinroleremove(self, ctx):
-        try:
-            role_id = int(str(ctx.message.content).split(" ")[1][3:-1])
-        except:
-            await ctx.send("you forgot the role")
+    async def joinroleremove(self, ctx, role: discord.Role = None):
+        if role == None:
+            embed = discord.Embed(title="Provide a role", color=0xFD3333)
+            await ctx.send(embed=embed)
+            return
+        if ctx.author.top_role.position < role.position:
+            await ctx.send(embed=discord.Embed(title="You dont have the permissions to remve this role", color=0xFD3333))
             return
 
-        guild_id = ctx.message.guild.id
-        with open('/tmp/discordbot/management/joinrole.json', 'r+') as f:
-            joinrole = json.load(f)
-            if str(guild_id) in joinrole.keys():
-                print("exist")
-                if role_id in joinrole[str(guild_id)]:
-                    if len(joinrole[str(guild_id)]) > 1:
-                        joinrole[str(guild_id)].remove(role_id)
-                        with open('/tmp/discordbot/management/joinrole.json', 'w') as file:
-                            json.dump(joinrole, file, indent=4)
-                    else:
-                        del joinrole[str(guild_id)]
-                        with open('/tmp/discordbot/management/joinrole.json', 'w') as file:
-                            json.dump(joinrole, file, indent=4)
-                    await ctx.send("This role is removed")
-                else:
-                    await ctx.send("Channel not added")
-            else:
-                await ctx.send("Channel not added")
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        config = collection.find_one(myquery)["config"]
+
+        if role.id in config["joinrole"]:
+            config["joinrole"].remove(role.id)
+            await ctx.send(embed=discord.Embed(title="Role removed from joinrole", color=0x00FF42))
+        else:
+            await ctx.send(embed=discord.Embed(title="Role not in joinrole", color=0xFD3333))
+            return
+
+        newvalue = {"$set": {"config": config}}
+        collection.update_one(myquery, newvalue)
 
 
     @commands.command(pass_context=True, aliases=['jrl', 'jrlist'])
     async def joinrolelist(self, ctx):
-        guild = ctx.guild
-        embed=discord.Embed(title="Join roles", color=0xFFF4E6)
-        with open('/tmp/discordbot/management/joinrole.json', 'r+') as f:
-            joinrole = json.load(f)
-            if str(guild.id) in joinrole.keys():
-                for role_id in joinrole[str(guild.id)]:
-                    role = get(guild.roles, id=role_id)
-                    embed.add_field(name=role.name, value="\u200b", inline=False)
-            else:
-                embed.add_field(name="Mission failed", value="There are no join roles on this server", inline=False)
-        await ctx.send(embed=embed)
-
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        config = collection.find_one(myquery)["config"]
+        if len(config["joinrole"]) != 0:
+            embed = discord.Embed(title="Join roles", color=0xFFF4E6)
+            for role_id in config["joinrole"]:
+                role = get(ctx.guild.roles, id=role_id)
+                embed.add_field(
+                    name=role.name, value="\u200b", inline=False)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(embed=discord.Embed(title="There are no join roles on this server", color=0xFD3333))
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        guild = member.guild
-        with open('/tmp/discordbot/management/joinrole.json', 'r+') as f:
-            joinrole = json.load(f)
-            if str(guild.id) in joinrole.keys():
-                for role_id in joinrole[str(guild.id)]:
-                    role = get(guild.roles, id=role_id)
-                    await member.add_roles(role)
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": member.guild.id}
+        config = collection.find_one(myquery)["config"]
+        if len(config["joinrole"]) != 0:
+            for role_id in config["joinrole"]:
+                role = get(member.guild.roles, id=role_id)
+                print(role)
+                await member.add_roles(role)
 
 # Reaction roles
 
@@ -232,7 +221,6 @@ class Admin(commands.Cog):
         await bot_message.add_reaction(emoji)
         await ctx.message.delete()
 
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         channel = await self.bot.fetch_channel(payload.channel_id)
@@ -244,7 +232,6 @@ class Admin(commands.Cog):
             role = str(message.content)[6:].split(" |")[0]
             role = discord.utils.get(guild.roles, name=role)
             await payload.member.add_roles(role)
-
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
@@ -259,7 +246,6 @@ class Admin(commands.Cog):
             member = await guild.fetch_member(payload.user_id)
             await member.remove_roles(role)
 
-    
     @commands.command(pass_context=True, aliases=['c'])
     async def reactionrolesclean(self, ctx):
         await ctx.message.delete()
@@ -269,13 +255,11 @@ class Admin(commands.Cog):
         for message in messages:
             if message.content[0:6] == "Role: " and message.author == ctx.guild.get_member(self.bot.user.id) and message.reactions != []:
                 tosend.insert(0, {"content": message.content,
-                               "reaction": message.reactions[0]})
+                                  "reaction": message.reactions[0]})
                 await message.delete()
         for sending in tosend:
             msg = await ctx.send(sending["content"])
             await msg.add_reaction(sending["reaction"])
-
-
 
 
 def setup(bot):

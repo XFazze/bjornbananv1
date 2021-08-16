b824ee1bf4d134faef1ca7dd625302c8ad0c0d15
xfazze <a@fabbe90.gq>
2021-08-16 22:45:37 +0200

    deletechannel updated to mongodb

--- newcogs/channels.py
+++ newcogs/channels.py
@@ -8,25 +8,13 @@ import os
 import pymongo as pm
 
 
-
-
-
-
-
 class Channels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.cleanse.start()
         self.hidechannels.start()
-        
 
-
-
-
-
-
-    # Delete pinned
-
+# Delete pinned
     @commands.command(pass_context=True, aliases=['edp'])
     @commands.has_permissions(manage_messages=True)
     async def enabledeletepinned(self, ctx):
@@ -40,7 +28,6 @@ class Channels(commands.Cog):
                 await ctx.send("Added channel to delete_pinned")
                 with open('/tmp/discordbot/management/delete_pinned.json', 'w') as file:
                     json.dump(delete_pinned, file, indent=4)
-
 
     @commands.command(pass_context=True, aliases=['ddp'])
     @commands.has_permissions(manage_messages=True)
@@ -56,7 +43,6 @@ class Channels(commands.Cog):
             else:
                 await ctx.send("This channel isnta a delete_pinned channel")
 
-
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if ctx.type.value == 6:
@@ -66,70 +52,66 @@ class Channels(commands.Cog):
                     if channel == ctx.channel.id:
                         await ctx.delete()
 
-
-
-
-
-
-
-
     # Deleting channel
 
     @commands.command(pass_context=True, aliases=['ed'])
     @commands.has_permissions(manage_messages=True)
     async def enabledelete(self, ctx):
-        with open('/tmp/discordbot/management/deletingchannel.json', 'r+') as f:
-            deletingchannel = json.load(f)
-            c_id = ctx.channel.id
-            if c_id in deletingchannel:
-                await ctx.send("This channel is already added to deletingchannel")
-            else:
-                deletingchannel.append(c_id)
-                await ctx.send("Added channel to deletingchannel")
-                with open('/tmp/discordbot/management/deletingchannel.json', 'w') as file:
-                    json.dump(deletingchannel, file, indent=4)
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        config = collection.find_one(myquery)["config"]
 
+        if ctx.channel.id in config["deletingchannel"]:
+            embed = discord.Embed(
+                title="Channel already decided, remove if changing", color=0xFD3333)
+            await ctx.send(embed=embed)
+
+        else:
+            config["deletingchannel"].append(ctx.channel.id)
+            newvalue = {"$set": {"config": config}}
+            collection.update_one(myquery, newvalue)
+            embed = discord.Embed(
+                title="Added channel to deletingchannel", color=0x00FF42)
+            await ctx.send(embed=embed)
 
     @commands.command(pass_context=True, aliases=['dd'])
     @commands.has_permissions(manage_messages=True)
     async def disabledelete(self, ctx):
-        with open('/tmp/discordbot/management/deletingchannel.json', 'r+') as f:
-            deletingchannel = json.load(f)
-            c_id = ctx.channel.id
-            if c_id in deletingchannel:
-                deletingchannel.remove(c_id)
-                await ctx.send("removed channel from deletingchannel")
-                with open('/tmp/discordbot/management/deletingchannel.json', 'w') as file:
-                    json.dump(deletingchannel, file, indent=4)
-            else:
-                await ctx.send("This channel isnta a deletingchannel channel")
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        config = collection.find_one(myquery)["config"]
 
+        if ctx.channel.id in config["deletingchannel"]:
+            config["deletingchannel"].remove(ctx.channel.id)
+            newvalue = {"$set": {"config": config}}
+            collection.update_one(myquery, newvalue)
+            embed = discord.Embed(
+                title="Removing channel from deletingchannel", color=0x00FF42)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="Channel isn't in deletingchannel", color=0xFD3333)
+            await ctx.send(embed=embed)
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=5) 
     async def cleanse(self):
-        with open('/tmp/discordbot/management/deletingchannel.json', 'r') as f:
-            deletingchannel = json.load(f)
-            if deletingchannel:
-                for channel in deletingchannel:
-                    channel = self.deletingchannel.get_channel(channel)
-                    messages = await channel.history(limit=100).flatten()
-                    await channel.delete_messages(messages)
-
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        guilds = collection.find({})
+        for guild in guilds:
+            guild_object = self.bot.get_guild(guild["id"])
+            if len(guild["config"]["deletingchannel"]) != 0:
+                for channelid in guild["config"]["deletingchannel"]:
+                    channel_obj = get(self.bot.get_all_channels(), id=channelid)
+                    messages = await channel_obj.history(limit=100).flatten()
+                    await channel_obj.delete_messages(messages)
+       
 
     @cleanse.before_loop
     async def before_cleanse(self):
         print('deletingchannel enabled')
-        await self.cleanse.wait_until_ready()
+        await self.bot.wait_until_ready()
 
+    # bettervc
 
-
-
-
-
-
-
-
-    # Better vc
 
     @commands.command(pass_context=True, aliases=['eb'])
     @commands.has_permissions(manage_roles=True)
@@ -152,7 +134,6 @@ class Channels(commands.Cog):
                 title="Added category to bettervc", color=0x00FF42)
             await ctx.send(embed=embed)
 
-
     @commands.command(pass_context=True, aliases=['db'])
     @commands.has_permissions(manage_roles=True)
     async def disablebettervc(self, ctx):
@@ -170,7 +151,7 @@ class Channels(commands.Cog):
         else:
             await ctx.send("category isn't in bettervc")
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=5)
     async def hidechannels(self):
         collection = MongoClient('localhost', 27017).maindb.guilds
         guilds = collection.find({})
@@ -187,7 +168,11 @@ class Channels(commands.Cog):
                     for hiding_channel in empty_channels:
                         await hiding_channel.set_permissions(guild_object.default_role, read_messages=False)
 
-
+    @hidechannels.before_loop
+    async def before_hidechannels(self):
+        print("Bettervc enabled")
+        await self.bot.wait_until_ready()
+        
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         collection = MongoClient('localhost', 27017).maindb.guilds
@@ -201,18 +186,7 @@ class Channels(commands.Cog):
                     break
 
 
-    @hidechannels.before_loop
-    async def before_hidechannels(self):
-        await self.bot.wait_until_ready()
-
-
-
-
-
-
-
-
-    # Join - Leave message
+    # joinleavemessage 
 
     @commands.command(pass_context=True, aliases=['ejlm'])
     @commands.has_permissions(manage_messages=True)
@@ -234,14 +208,13 @@ class Channels(commands.Cog):
                 with open('/tmp/discordbot/management/joinleavemessage.json', 'w') as file:
                     json.dump(joinleavemessage, file, indent=4)
 
-
     @commands.command(pass_context=True, aliases=['djlm'])
     @commands.has_permissions(manage_messages=True)
     async def disablejoinleavemessage(self, ctx):
         await ctx.message.delete()
         with open('/tmp/discordbot/management/joinleavemessage.json', 'r+') as f:
             joinleavemessage = json.load(f)
-            if str(ctx.guild.id) in  joinleavemessage.keys():
+            if str(ctx.guild.id) in joinleavemessage.keys():
                 if ctx.channel.id in joinleavemessage[str(ctx.guild.id)]:
                     joinleavemessage[str(ctx.guild.id)].remove(ctx.channel.id)
                     await ctx.send("removed channel from joinleavemessage")
@@ -250,7 +223,6 @@ class Channels(commands.Cog):
                     return
             await ctx.send("Channel not in joinleavemessage")
 
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
         with open('/tmp/discordbot/management/joinleavemessage.json', 'r+') as f:
@@ -258,20 +230,20 @@ class Channels(commands.Cog):
             directory = os.fsencode('/tmp/discordbot/logs/joinleave_logs/')
             times = 0
             for file in os.listdir(directory):
-                filename = '/tmp/discordbot/logs/joinleave_logs/'+os.fsdecode(file)
+                filename = '/tmp/discordbot/logs/joinleave_logs/' + \
+                    os.fsdecode(file)
                 with open(filename, 'r') as file:
                     filelines = file.readlines()
                     for line in filelines:
                         if "join" in line and str(member) in line:
                             times += 1
 
-
             if str(member.guild.id) in joinleavemessage.keys():
                 for channel in joinleavemessage[str(member.guild.id)]:
                     channel = self.bot.get_channel(channel)
-                    embed = discord.Embed(title=str(member)+ "  joined " + str(times)+" times" , description=time.asctime() , color=0x00ff00)
+                    embed = discord.Embed(title=str(
+                        member) + "  joined " + str(times)+" times", description=time.asctime(), color=0x00ff00)
                     await channel.send(embed=embed)
-
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
@@ -280,25 +252,20 @@ class Channels(commands.Cog):
             directory = os.fsencode('/tmp/discordbot/logs/joinleave_logs/')
             times = 0
             for file in os.listdir(directory):
-                filename = '/tmp/discordbot/logs/joinleave_logs/'+os.fsdecode(file)
+                filename = '/tmp/discordbot/logs/joinleave_logs/' + \
+                    os.fsdecode(file)
                 with open(filename, 'r') as file:
                     filelines = file.readlines()
                     for line in filelines:
                         if "leave" in line and str(member) in line:
                             times += 1
-                
+
             if str(member.guild.id) in joinleavemessage.keys():
                 for channel in joinleavemessage[str(member.guild.id)]:
                     channel = self.bot.get_channel(channel)
-                    embed = discord.Embed(title=str(member) + "  left " + str(times)+" times", description=time.asctime(), color=0xFF0000)
+                    embed = discord.Embed(title=str(
+                        member) + "  left " + str(times)+" times", description=time.asctime(), color=0xFF0000)
                     await channel.send(embed=embed)
-
-
-
-
-
-
-
 
     # Role log
 
@@ -316,8 +283,7 @@ class Channels(commands.Cog):
                 await ctx.send("Added channel to rolelog")
                 with open('/tmp/discordbot/management/rolelog.json', 'w') as file:
                     json.dump(rolelog, file, indent=4)
-    
-    
+
     @commands.command(pass_context=True, aliases=['dr'])
     @commands.has_permissions(manage_roles=True)
     async def disablerolelog(self, ctx):
@@ -332,8 +298,7 @@ class Channels(commands.Cog):
                     json.dump(rolelog, file, indent=4)
             else:
                 await ctx.send("This channel isnta a rolelog channel")
-    
-    
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         if before.roles == after.roles:
@@ -348,34 +313,18 @@ class Channels(commands.Cog):
                 channel = self.bot.get_channel(channel_id)
                 if len(before.roles) > len(after.roles):
                     role = list(set(before.roles)-set(after.roles))[0]
-                    message = "User: "+ str(before) +"\nRole removed: " +role.name
+                    message = "User: " + \
+                        str(before) + "\nRole removed: " + role.name
                     await channel.send(message)
                 else:
                     role = list(set(after.roles)-set(before.roles))[0]
                     if role.id == 802300001875001455:
-                        message = "@everyone User: "+ str(before) +"\nRole added: " +role.name
+                        message = "@everyone User: " + \
+                            str(before) + "\nRole added: " + role.name
                     else:
-                        message = "User: "+ str(before) +"\nRole added: " +role.name
+                        message = "User: " + \
+                            str(before) + "\nRole added: " + role.name
                     await channel.send(message)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

7188896f40852eacef088235ef23de86f136c5cc
Noel Kleen <noelhkleen@gmail.com>
2021-08-16 14:56:14 +0200

    g

--- cogs/dev/git.py
+++ cogs/dev/git.py
@@ -11,12 +11,15 @@ class Git(commands.Cog):
 
     @commands.command(pass_context=True)
     async def git(self, ctx, action = None):
+        if not str(ctx.author) == "mega#2222" and  not str(ctx.author) == "AbstractNucleus#6969":
+            await ctx.send("You're noone")
+            return
+        
+            
         if action == None:
             await ctx.send("Specify an action")
-        elif action == "pull":
-            print("bitchhhh")
-            #result = g.pull()
-            #print(result)
+        else:
+            await ctx.send("Success")
     
 def setup(bot):
     bot.add_cog(Git(bot))

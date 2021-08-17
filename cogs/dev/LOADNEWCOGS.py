b1934e9d42ef0b90ca430c395fdcec0679b4c987
xfazze <a@fabbe90.gq>
2021-08-17 09:06:54 +0200

    unloadold

--- cogs/dev/LOADNEWCOGS.py
+++ cogs/dev/LOADNEWCOGS.py
@@ -18,6 +18,42 @@ class Cog_manager(commands.Cog):
         for extension in self.bot.extensions:
             await ctx.send(extension)
 
+    @commands.command(pass_context=True)
+    async def unloadold(self, ctx):
+        await ctx.message.delete()
+        if not str(ctx.author) == "mega#2222" and not str(ctx.author) == "AbstractNucleus#6969":
+            await ctx.send("Youre noone")
+            return
+
+        extensions = []
+        for extension in self.bot.extensions:
+            extensions.append(extension.split(
+                '.')[1] + '.' + extension.split('.')[2]+'.py')
+
+        if len(os.listdir('./cogs')) > 1:
+            for f in os.listdir('./cogs'):
+                if f.endswith('.py'):
+                    continue
+
+                title = str(f)
+                for f in os.listdir('./cogs/'+title):
+                    if f.endswith('.py'):
+                        cog = f"cogs.{title}.{str(f)[:-3]}"
+                        if str(f) == 'uptime.py' or str(f) == 'cog_manager.py' or str(f) == 'git.py' :
+                            continue
+                       
+                        try:
+                            self.bot.unload_extension(cog)
+                        except:
+                            embed = discord.Embed(
+                                title="Broken cog: " + cog, color=0xFD3333)
+                            await ctx.send(embed=embed)
+
+
+        embed = discord.Embed(title="Reloaded "+" all cogs", color=0x00FF42)
+        await ctx.send(embed=embed)
+
+
    
 
 def setup(bot):

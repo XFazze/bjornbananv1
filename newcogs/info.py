2cb29fa8f8eae2a8430bb74d9ac75af305aa3621
xfazze <a@fabbe90.gq>
2021-08-17 10:51:07 +0200

    increased bot info command

--- newcogs/info.py
+++ newcogs/info.py
@@ -144,7 +144,7 @@ class Info(commands.Cog):
     
 # Bot info
     @commands.command(pass_context=True, aliases=['botinfo', 'bot'])
-    async def app_info(self, ctx):
+    async def appinfo(self, ctx):
         res = await self.bot.application_info()
         embed=discord.Embed(title=res.name, description=res.description, color=0x00FF42)
         if res.bot_public:
@@ -152,7 +152,13 @@ class Info(commands.Cog):
         else:
             embed.add_field(name="Private bot Owner: ", value=res.owner.name+"#"+res.owner.discriminator)
         embed.add_field(name='Id', value=res.id, inline=False)
-
+        embed.add_field(name='Guilds', value=len(self.bot.guilds), inline=False)
+        embed.add_field(name='Users', value=len(self.bot.users), inline=False)
+        embed.add_field(name='Commands', value=len(self.bot.commands), inline=False)
+        embed.add_field(name='Emojis', value=len(self.bot.emojis), inline=False)
+        embed.add_field(name='Latency', value=self.bot.latency, inline=False)
+        embed.add_field(name='Private channels', value=len(self.bot.private_channels), inline=False)
+        embed.add_field(name='Voice clients', value=len(self.bot.voice_clients), inline=False)
         await ctx.send(embed=embed)
 # Ping
     

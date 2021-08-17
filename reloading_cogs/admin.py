4a641904ec05525393e6b7a5be58328a689f571d
xfazze <a@fabbe90.gq>
2021-08-17 12:20:24 +0200

    added setting key to guild collection

--- reloading_cogs/admin.py
+++ reloading_cogs/admin.py
@@ -16,68 +16,41 @@ class Admin(commands.Cog):
 
 # Enable/disable command
 
-    @commands.command(pass_context=True, enabled=False)
-    @commands.has_permissions(manage_roles=True)
-    async def adisable(self, ctx):
-        try:
-            command = str(ctx.message.content).split(" ")[1]
-        except:
-            await ctx.send("Provide a command")
-        if command in ["enable", "disable"]:
-            await ctx.send("cant disable this command")
-        for botcommand in self.bot.commands:
-            if command == str(botcommand):
-                guild_id = ctx.guild.id
-                with open('/tmp/discordbot/management/enable.json', 'r+') as f:
-                    enable = json.load(f)
-                    if str(guild_id) in enable.keys():
-                        if command in enable[str(guild_id)]:
-                            await ctx.send("This command is already disabled")
-                        else:
-                            enable[str(guild_id)].append(command)
-                            await ctx.send("Disabled command")
-                            with open('/tmp/discordbot/management/enable.json', 'w') as file:
-                                json.dump(enable, file, indent=4)
-                    else:
-                        enable[int(guild_id)] = [command]
-                        await ctx.send("Disabled command")
-                        with open('/tmp/discordbot/management/enable.json', 'w') as file:
-                            json.dump(enable, file, indent=4)
+    @commands.group(pass_context=True)
+    @commands.has_permissions(manage_guild=True)
+    async def disable(self, ctx):
+        if ctx.invoked_subcommand is None:
+            if len(ctx.message.content.split(" ")) == 1:
+                await ctx.send(embed=discord.Embed(title="Provide a command", color=0xFD3333))
+                return
+
+            command = ctx.message.content.split(" ")[1]
+            print(self.bot.commands)
+            validcommand= False
+            for cmd in self.bot.commands:
+                if command == cmd.name:
+                    validcommand = True
+                    break
+            if not validcommand:
+                await ctx.send(embed=discord.Embed(title="Provide a valid command", color=0xFD3333))
+
+            
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        settings = collection.find_one(myquery)["settings"]
+
+    @disable.command(pass_context=True)
+    async def category(self, ctx, category: discord.CategoryChannel = None, command: str = None):
+        print("disabling category ", category, command)
+
+    @disable.command(pass_context=True)
+    async def channel(self, ctx, channel: discord.TextChannel = None, command: str = None):
+        print("disabling channel ", channel, command)
 
     @commands.command(pass_context=True, enabled=False)
-    @commands.has_permissions(manage_roles=True)
-    async def aenable(self, ctx):
-        try:
-            command = str(ctx.message.content).split(" ")[1]
-        except:
-            await ctx.send("Provide a command")
-        if command in ["enable", "disable"]:
-            await ctx.send("cant disable this command")
-        for botcommand in self.bot.commands:
-            if command == str(botcommand):
-                guild_id = ctx.guild.id
-                with open('/tmp/discordbot/management/enable.json', 'r+') as f:
-                    enable = json.load(f)
-                    if str(guild_id) in enable.keys():
-                        print("exist")
-                        if command in enable[str(guild_id)]:
-                            if len(enable[str(guild_id)]) > 1:
-                                enable[str(guild_id)].remove(command)
-                                with open('/tmp/discordbot/management/enable.json', 'w') as file:
-                                    json.dump(enable, file, indent=4)
-                            else:
-                                del enable[str(guild_id)]
-                                with open('/tmp/discordbot/management/enable.json', 'w') as file:
-                                    json.dump(enable, file, indent=4)
-                            await ctx.send("This command is enabled")
-                            return
-                        else:
-                            await ctx.send("Command already allowed")
-                            return
-                    else:
-                        await ctx.send("Command already allowed")
-                        return
-        await ctx.send("Command does not exist")
+    @commands.has_permissions(manage_guild=True)
+    async def enable(self, ctx):
+        pass
 
 # Björnbanan set prefix
 
@@ -157,7 +130,6 @@ class Admin(commands.Cog):
 
         newvalue = {"$set": {"config": config}}
         collection.update_one(myquery, newvalue)
-
 
     @commands.command(pass_context=True, aliases=['jrl', 'jrlist'])
     async def joinrolelist(self, ctx):

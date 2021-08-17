862f7e82b8aa41a09476fa7edbb3483ee5b5ba93
xfazze <a@fabbe90.gq>
2021-08-17 13:07:54 +0200

    f

--- newcogs/dev.py
+++ newcogs/dev.py
@@ -169,60 +169,60 @@ class Dev(commands.Cog):
                     "name": guild.name
                 }
 
-                text_channels = []
-                for channel in guild.text_channels:
-                    if channel.category != None:
-                        category = channel.category.name
-                        category_id = channel.category_id
-                    else:
-                        category = None
-                        category_id = None
+            text_channels = []
+            for channel in guild.text_channels:
+                if channel.category != None:
+                    category = channel.category.name
+                    category_id = channel.category_id
+                else:
+                    category = None
+                    category_id = None
 
-                    text_channel = {
-                        "id": channel.id,
-                        "name": channel.name,
-                        "category": category_id,
-                        "category_id": category_id,
+                text_channel = {
+                    "id": channel.id,
+                    "name": channel.name,
+                    "category": category_id,
+                    "category_id": category_id,
 
-                        }
-                    text_channels.append(text_channel)
+                    }
+                text_channels.append(text_channel)
 
-                voice_channels = []
-                for channel in guild.voice_channels:
-                    if channel.category != None:
-                        category = channel.category.name
-                        category_id = channel.category_id
-                    else:
-                        category = None
-                        category_id = None
-                    voice_channel = {
-                        "id": channel.id,
-                        "name": channel.name,
-                        "category": category_id,
-                        "category_id": category_id,
-                        }
-                    voice_channels.append(voice_channel)
+            voice_channels = []
+            for channel in guild.voice_channels:
+                if channel.category != None:
+                    category = channel.category.name
+                    category_id = channel.category_id
+                else:
+                    category = None
+                    category_id = None
+                voice_channel = {
+                    "id": channel.id,
+                    "name": channel.name,
+                    "category": category_id,
+                    "category_id": category_id,
+                    }
+                voice_channels.append(voice_channel)
 
 
-                doc["text_channels"] = text_channels
-                doc["voice_channels"] = voice_channels
-                if "config" not in doc.keys():
-                    doc["config"] = {}
-                if "joinrole" not in doc["config"].keys():
-                    doc["config"]["joinrole"] = []
-                if "prefix" not in doc["config"].keys():
-                    doc["config"]["prefix"] = ','
-                if "bettervc" not in doc["config"].keys():
-                    doc["config"]["bettervc"] = []
-                if "delete_pinned" not in doc["config"].keys():
-                    doc["config"]["delete_pinned"] = []
-                if "deletingchannel" not in doc["config"].keys():
-                    doc["config"]["deletingchannel"] = []
-                if "joinleavemessage" not in doc["config"].keys():
-                    doc["config"]["joinleavemessage"] = []
+            doc["text_channels"] = text_channels
+            doc["voice_channels"] = voice_channels
+            if "config" not in doc.keys():
+                doc["config"] = {}
+            if "joinrole" not in doc["config"].keys():
+                doc["config"]["joinrole"] = []
+            if "prefix" not in doc["config"].keys():
+                doc["config"]["prefix"] = ','
+            if "bettervc" not in doc["config"].keys():
+                doc["config"]["bettervc"] = []
+            if "delete_pinned" not in doc["config"].keys():
+                doc["config"]["delete_pinned"] = []
+            if "deletingchannel" not in doc["config"].keys():
+                doc["config"]["deletingchannel"] = []
+            if "joinleavemessage" not in doc["config"].keys():
+                doc["config"]["joinleavemessage"] = []
 
-                if "settings" not in doc.keys():
-                    doc["settings"] = {}
+            if "settings" not in doc.keys():
+                doc["settings"] = {}
 
             if create:
                 collection.insert_one(doc)

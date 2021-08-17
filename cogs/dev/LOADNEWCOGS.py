ba4a740d0704c00c7ac5fbaddc73c2dc553513e0
xfazze <a@fabbe90.gq>
2021-08-17 09:03:02 +0200

    added extension shower

--- cogs/dev/LOADNEWCOGS.py
+++ cogs/dev/LOADNEWCOGS.py
@@ -13,5 +13,13 @@ class Cog_manager(commands.Cog):
         await ctx.send("loaded newcogs.cog_manager")
 
 
+    @commands.command(pass_context=True)
+    async def sex(self, ctx):
+        self.bot.load_extension("newcogs.cog_manager")
+        for extension in self.bot.extensions:
+            await ctx.send(extension)
+
+   
+
 def setup(bot):
     bot.add_cog(Cog_manager(bot))

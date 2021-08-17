642d3c2f4f953d5e2b7c0a9dc2187b197b0c7899
Noel Kleen <noelhkleen@gmail.com>
2021-08-17 22:15:23 +0200

    a

--- newcogs/utilities.py
+++ newcogs/utilities.py
@@ -45,48 +45,20 @@ class Utilities(commands.Cog):
 
 
 # Clear   
-    @commands.command(pass_context=True, aliases=['g', 'purge'])
-    @commands.has_permissions(manage_messages=True)
-    async def clear(self, ctx):
-        await ctx.message.delete()
-        try:
-            message = ctx.message.content.split(" ")
-            amount = int(message[1])
-            print(f"asked to delete {amount} messages by {ctx.message.author}")
-            await ctx.message.delete()
-        except:
-            await ctx.send('provide a valid number("gclear 2")')
-            return
-        try:
-            if message[2] == "not":
-                try:
-                    note = int(message[3])
-                except:
-                    await ctx.send('You need to provide a number ("gclear x not z" z can be 1 or 5 etc )')
-                    return
-
-        except:
-            note = 0
-        if amount > 99:
-            secret = await ctx.send('You executed the secret protocol')
-            for x in range(math.floor(amount/99)):
-                messages = await ctx.message.channel.history(limit=101+note).flatten()
-                for _ in range(note+1):
-                    messages.pop(0)
-                await ctx.message.channel.delete_messages(messages)
-            amount = amount % 99
-            await secret.delete()
-        messages = await ctx.message.channel.history(limit=amount+note).flatten()
-        if note:
-            print(len(messages))
-            for _ in range(note):
-                messages.pop(0)
-
-        await ctx.message.channel.delete_messages(messages)
-
-        message = await ctx.send(f"I have deleted {amount} messages for you master")
-        time.sleep(2)
-        await message.delete()
+    @commands.command(aliases = ['purge','delete'], usage="clear [int]")
+    @commands.has_permissions(manage_messages = True)
+    async def clear(self, ctx, amount = None):
+        if amount == None:
+            embed = discord.Embed(title=f'Usage: `{self.bot.get_command("clear").usage}`')
+            await ctx.send(embed=embed)
+        else:
+            try:
+                int(amount)
+            except:
+                embed = discord.Embed(title=f'Usage: `{self.bot.get_command("clear").usage}`')
+                await ctx.send(embed=embed)
+            else:
+                await ctx.channel.purge(limit=amount)
 
 
 # Color code 

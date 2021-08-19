41bf90099f163aefe40772d24470529156d86b02
xfazze <a@fabbe90.gq>
2021-08-19 19:05:23 +0200

    buttons added and tested with

--- ebot.py
+++ ebot.py
@@ -4,20 +4,23 @@ import re
 import discord
 from discord.ext import commands
 from cogwatch import Watcher
+from discord_components import Button, Select, SelectOption, ComponentsBot, DiscordComponents
 
 
-bot = commands.Bot(command_prefix="=", intents=discord.Intents.all())   
+bot = commands.Bot(command_prefix="=", intents=discord.Intents.all())
 bot.remove_command('help')
 
 
 # Gets the token
-token1, error = subprocess.Popen(["cat", "/tmp/discordbot/secrets.txt"], stdout=subprocess.PIPE).communicate()
+token1, error = subprocess.Popen(
+    ["cat", "/tmp/discordbot/secrets.txt"], stdout=subprocess.PIPE).communicate()
 token1 = re.split("'", str(token1))
 token = token1[1].split(" ")
 
 
 @bot.event
 async def on_ready():
+    DiscordComponents(bot)
     print("Logged in as: " + bot.user.name)
     watcher = Watcher(bot, path="reloading_cogs", preload=True)
     await watcher.start()
--- reloading_cogs/folder/managecommands.py
+++ reloading_cogs/folder/managecommands.py
@@ -1,17 +1,21 @@
 import discord
+from discord import embeds
 from discord.ext import commands
+from discord.ext.commands.core import command
 from pymongo import MongoClient, collation
+from discord_components import Button, Select, SelectOption, ComponentsBot
+from discord.utils import get
 
 
 class managecommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     # Enable/disable command
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def disable(self, ctx, command:str=None, role:discord.Role = None):
+    async def disable(self, ctx, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -43,7 +47,6 @@ class managecommands(commands.Cog):
             await ctx.send(embed=discord.Embed(title="Command is already disabled", color=0xFD3333))
             return
 
-        
         if role.id in settings[command]['guild']:
             settings[command]['guild'].remove(role.id)
         newvalue = {"$set": {"settings": settings}}
@@ -52,7 +55,7 @@ class managecommands(commands.Cog):
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def disablecategory(self, ctx, category: discord.CategoryChannel = None, command: str = None , role:discord.Role = None):
+    async def disablecategory(self, ctx, category: discord.CategoryChannel = None, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -80,29 +83,28 @@ class managecommands(commands.Cog):
             }
 
         if str(category.id) not in settings[command]['disabled_category'].keys():
-            settings[command]['disabled_category'][str(category.id)] = [role.id]
+            settings[command]['disabled_category'][str(category.id)] = [
+                role.id]
 
         else:
             if role.id in settings[command]['disabled_category'][str(category.id)]:
                 await ctx.send(embed=discord.Embed(title="Command is already disabled", color=0xFD3333))
                 return
             else:
-                settings[command]['disabled_category'][str(category.id)].append(role.id)
+                settings[command]['disabled_category'][str(
+                    category.id)].append(role.id)
 
         if str(category.id) in settings[command]['category'].keys():
             if role.id in settings[command]['category'][str(category.id)]:
                 settings[command]['category'][str(category.id)].remove(role.id)
 
-
         newvalue = {"$set": {"settings": settings}}
         collection.update_one(myquery, newvalue)
-        await ctx.send(embed=discord.Embed(title="Disabled "+command+" in category "+ category.name+" for "+role.name + category.name, color=0x00FF42))
-        
-            
+        await ctx.send(embed=discord.Embed(title="Disabled "+command+" in category " + category.name+" for "+role.name + category.name, color=0x00FF42))
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def disablechannel(self, ctx, channel: discord.TextChannel = None, command: str = None , role:discord.Role = None):
+    async def disablechannel(self, ctx, channel: discord.TextChannel = None, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -137,20 +139,20 @@ class managecommands(commands.Cog):
                 await ctx.send(embed=discord.Embed(title="Command is already disabled", color=0xFD3333))
                 return
             else:
-                settings[command]['disabled_channel'][str(channel.id)].append(role.id)
+                settings[command]['disabled_channel'][str(
+                    channel.id)].append(role.id)
 
         if str(channel.id) in settings[command]['channel'].keys():
             if role.id in settings[command]['channel'][str(channel.id)]:
                 settings[command]['channel'][str(channel.id)].remove(role.id)
 
-
         newvalue = {"$set": {"settings": settings}}
         collection.update_one(myquery, newvalue)
-        await ctx.send(embed=discord.Embed(title="Disabled "+command+" in channel "+ channel.name+" for "+role.name, color=0x00FF42))
+        await ctx.send(embed=discord.Embed(title="Disabled "+command+" in channel " + channel.name+" for "+role.name, color=0x00FF42))
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def enable(self, ctx, command:str=None, role:discord.Role = None):
+    async def enable(self, ctx, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -181,7 +183,7 @@ class managecommands(commands.Cog):
         else:
             await ctx.send(embed=discord.Embed(title="Command is already enabled", color=0xFD3333))
             return
-        
+
         if role.id in settings[command]['disabled_guild']:
             settings[command]['disabled_guild'].remove(role.id)
 
@@ -191,7 +193,7 @@ class managecommands(commands.Cog):
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def enablecategory(self, ctx, category: discord.CategoryChannel = None, command: str = None, role:discord.Role = None):
+    async def enablecategory(self, ctx, category: discord.CategoryChannel = None, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -230,16 +232,16 @@ class managecommands(commands.Cog):
 
         if str(category.id) in settings[command]['disabled_category'].keys():
             if role.id in settings[command]['disabled_category'][str(category.id)]:
-                settings[command]['disabled_category'][str(category.id)].remove(role.id)
-
+                settings[command]['disabled_category'][str(
+                    category.id)].remove(role.id)
 
         newvalue = {"$set": {"settings": settings}}
         collection.update_one(myquery, newvalue)
-        await ctx.send(embed=discord.Embed(title="Enabled "+command+" in category " + category.name +" for "+role.name, color=0x00FF42))
+        await ctx.send(embed=discord.Embed(title="Enabled "+command+" in category " + category.name + " for "+role.name, color=0x00FF42))
 
     @commands.command(pass_context=True)
     @commands.has_permissions(manage_guild=True)
-    async def enablechannel(self, ctx, channel: discord.TextChannel = None, command: str = None, role:discord.Role = None):
+    async def enablechannel(self, ctx, channel: discord.TextChannel = None, command: str = None, role: discord.Role = None):
         validcommand = False
         for cmd in self.bot.commands:
             if command == cmd.name:
@@ -278,16 +280,44 @@ class managecommands(commands.Cog):
 
         if str(channel.id) in settings[command]['disabled_channel'].keys():
             if role.id in settings[command]['disabled_channel'][str(channel.id)]:
-                settings[command]['disabled_channel'][str(channel.id)].remove(role.id)
-
+                settings[command]['disabled_channel'][str(
+                    channel.id)].remove(role.id)
 
         newvalue = {"$set": {"settings": settings}}
         collection.update_one(myquery, newvalue)
-        await ctx.send(embed=discord.Embed(title="Enabled "+command+" in channel " + channel.name +" for "+role.name, color=0x00FF42))
+        await ctx.send(embed=discord.Embed(title="Enabled "+command+" in channel " + channel.name + " for "+role.name, color=0x00FF42))
 
     @commands.command(pass_context=True)
-    async def a(self, ctx):
-        await ctx.send("NOEEEL")
+    async def command(self, ctx):
+        collection = MongoClient('localhost', 27017).maindb.guilds
+        myquery = {"id": ctx.guild.id}
+        settings = collection.find_one(myquery)["settings"]
+        options=[]
+        for setting in settings.keys():
+            options.append(SelectOption(label=setting, value=setting))
+
+        message = await ctx.send("NOEEEL", components=[Select(placeholder="Select something!", options=options, custom_id="commandperms",)])
+        while True:
+            interaction = await self.bot.wait_for("select_option")
+            embed = discord.Embed(name="Command permissions for ", value=interaction.values[0])
+            if len(settings[interaction.values[0]]["guild"]) > 0:
+                msg = ""
+                for roleid in settings[interaction.values[0]]["guild"]:
+                    role_obj = get(ctx.guild.roles, id=roleid)
+                    msg += role_obj.name+'\n'
+            embed.add_field(name="GUILD WIDE allowed", value=msg)
+            if len(settings[interaction.values[0]]["guild"]) > 0:
+                msg = ""
+                for roleid in settings[interaction.values[0]]["disabled_guild"]:
+                    role_obj = get(ctx.guild.roles, id=roleid)
+                    msg += role_obj.name+'\n'
+            embed.add_field(name="GUILD WIDE denied", value=msg)
+
+
+                
+            await message.edit(embed=embed,components=[Select(placeholder="Select something!", options=options, custom_id="commandperms",)])
+
+
 
 def setup(bot):
     bot.add_cog(managecommands(bot))

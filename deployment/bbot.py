309f441326d629ed8fdaab895d34b11197919b1f
xfazze <a@fabbe90.gq>
2021-12-13 18:02:16 +0100

    formatting

--- deployment/bbot.py
+++ deployment/bbot.py
@@ -6,7 +6,8 @@ from pymongo import MongoClient
 # Set prefix here
 prefix = ","
 
-#FIXME editing prefix doesnt work
+# FIXME editing prefix doesnt work
+
 async def determine_prefix(bot, message):
     prefixes = json.load(open('/tmp/discordbot/management/prefixes.json', 'r'))
     guild = message.guild
@@ -17,10 +18,7 @@ async def determine_prefix(bot, message):
 
 # Removes default help command and creates the bot object
 bot = commands.Bot(command_prefix=',', intents=discord.Intents.all())
-#bot.remove_command('help')
-
-if __name__ == '__main__':
-    bot.load_extension('cogv3.dev.cog_manager')
+# bot.remove_command('help')
 
 
 # Gets the token
@@ -30,12 +28,16 @@ doc = collection.find_one(myquery)
 if not doc:
     raise ValueError('Token not found in mongodb database.')
 token = doc['token']
+
 # Creates the bot event
+
+
 @bot.event
 async def on_ready():
     print(f"\n\nLogged in as: {bot.user.name}\n")
-    prefix = ','
     await bot.change_presence(activity=discord.Game(name=f",help | fabbe90.gq"))
 
 
-bot.run(token)
+if __name__ == '__main__':
+    bot.load_extension('cogv3.dev.cog_manager')
+    bot.run(token)

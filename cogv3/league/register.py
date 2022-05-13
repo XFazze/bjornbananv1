aafe2ff54ddbaa0668051b60295f56bd8ac70cb4
xfazze <1339139+XFazze@users.noreply.github.com>
2022-05-13 20:01:03 +0200

    meddelande till nioel

--- cogv3/league/register.py
+++ cogv3/league/register.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 from ..admin.managecommands import perms
 
+# meddelande från fabian 
+# spara riot puuid, alltså inte användarnamnet
 
 class Register(commands.Cog):
     def __init__(self, bot):
